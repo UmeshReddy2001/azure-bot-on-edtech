48f12b7b481712452b399be842c9f55a021cf1e4
UmeshReddy2001 <109653220+UmeshReddy2001@users.noreply.github.com>
2022-08-30 17:35:01 +0530

    Add files via upload

---
+++ faqsonedtech-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\faqsonedtech-bot-9633-Web-Deploy.pubxml -p:Password=dqdLcltuHsnL0vYgSzllddFAKq4RfdwBaZ5l4yNtoFsTcunzm0y4zskpqmTu
+
